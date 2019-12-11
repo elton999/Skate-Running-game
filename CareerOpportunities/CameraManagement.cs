96e162025a83bb457add97d6d6bdf53fa30fbbca
Elton Silva <eltoncarsilva73@gmail.com>
2019-12-11 15:43:47 -0300

    camera class

---
+++ CareerOpportunities/CameraManagement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CareerOpportunities
+{
+    public class CameraManagement
+    {
+
+        public float TimeShake;
+        public Vector2 maxShake;
+
+        public CameraManagement()
+        {
+        }
+
+        public void Update(GameTime gametime)
+        {
+
+        }
+
+        private void UpdateShake(int delta)
+        { 
+        
+        }
+
+        private bool IsShake()
+        {
+
+            return false;
+        }
+    }
+}
